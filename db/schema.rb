b92bfc5071cf02c08dec65f15c093a9c8a6ac4b7
uanka <ankazeml97@gmail.com>
2018-01-04 23:10:52 +0200

    added a few gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104003044) do
+ActiveRecord::Schema.define(version: 20180104183411) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
